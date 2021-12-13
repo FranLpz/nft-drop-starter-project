272d47e7b2f7f2d93705f4d707172d5c24ed2123
FranLpz <franciscoalopezd@gmail.com>
2021-12-13 16:45:04 -0500

    Create button to connect wallet

--- app/src/App.js
+++ app/src/App.js
@@ -40,6 +40,26 @@ const App = () => {
   };
 
   /*
+  * Let's define this method so our code doesn't break.
+  * We will write the logic for this next!
+  */
+  const connectWallet = async () => {};
+
+  /*
+  * We want to render this UI when the user hasn't connected
+  * their wallet to our app yet.
+  */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+
+  /*
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
    */
@@ -57,6 +77,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* Render your connect to wallet button right here */}
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
