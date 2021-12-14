7d9b0935f48a4f69a4e98c30b33316479808f243
FranLpz <franciscoalopezd@gmail.com>
2021-12-13 19:56:34 -0500

    Add functionality to the button.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,12 +7,11 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  
   // Actions
-
-  /*
-  * Declare your function
-  */
-  const checkIfWalletIsConnected = async () => {
+  const checkIfWalletIsConnected = async () => {  
     try {
       const { solana } = window;
 
@@ -30,6 +29,8 @@ const App = () => {
             response.publicKey.toString()
           );
 
+          // Set the user's publicKey in state to be used later!
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -43,7 +44,15 @@ const App = () => {
   * Let's define this method so our code doesn't break.
   * We will write the logic for this next!
   */
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if(solana){
+      const response = await solana.connect()
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
 
   /*
   * We want to render this UI when the user hasn't connected
@@ -77,8 +86,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-          {/* Render your connect to wallet button right here */}
-          {renderNotConnectedContainer()}
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
